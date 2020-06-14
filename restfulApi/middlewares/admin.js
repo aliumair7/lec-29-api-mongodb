8ff1a2564062ec363e8cb8d35817e39229c4d21e
aliumair7 <60683372+aliumair7@users.noreply.github.com>
2020-06-14 10:55:13 +0500

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- restfulApi/middlewares/admin.js
+++
@@ -1,9 +0,0 @@
-
-function admin(req,res,next){
-    if( req.user.role !="admin") 
-    return res.status(400).send("Not Autherized")
-
-
-    next()
-}
-module.exports=admin;
--- restfulApi/middlewares/product_validate.js
+++
@@ -1,8 +0,0 @@
-var {validate}=require('../models/productmodel')
-function validaton(req,res,next){
-    let {error}=validate(req.body)
-    if (error) return res.send(error.details[0].message)
-    next()
-
-}
-module.exports=validaton;
